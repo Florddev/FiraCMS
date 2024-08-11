28621e9ce975563ac89998be6d099f8fd320a32e
Florddev <florddev@protonmail.com>
2024-08-11 18:19:12 +0200

    Suppression des plugins de /plugins

--- plugins/MonNouveauPlugin/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Plugins\MonNouveauPlugin\Controllers\PluginController;
-
-Route::middleware('auth')->prefix('MonNouveauPlugin')->group(function () {
-    Route::get('/test', 'PluginController@index')->name('mon-nouveau-plugin.index');
-});
